23834d7192d9d5904b28e78e2ae30d822b25e644
zx026 <yb131567@gmail.com>
2025-11-30 09:36:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,8 +22,8 @@ class Config:
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+Kn5hMdwlFfQ5ZTg1")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+p8q2ypJzqodmNTY1")
 
         self.AUTO_END: bool = getenv("AUTO_END", False)
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", False)
